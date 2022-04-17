6bbc78704b20509826affa73941ec967022e65ec
Jonnas Costa <jonn567@gmail.com>
2022-04-17 20:15:32 +0100

    add snake_game_tutorial to repo

---
+++ snake_game_tutorial/src/main.rs
@@ -0,0 +1,54 @@
+use bevy::prelude::*;
+
+
+#[derive(Component)]
+struct SnakeHead;
+const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
+fn main() {
+    App::new()
+    .add_startup_system(setup_camera)
+    .add_startup_system(spawn_snake)
+    .add_system(snake_movement) 
+    .add_plugins(DefaultPlugins)
+    .run();
+}
+
+fn setup_camera(mut commands: Commands){
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn spawn_snake(mut commands: Commands) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: SNAKE_HEAD_COLOR,
+                ..default()
+            },
+            transform: Transform {
+                scale: Vec3::new(10.0, 10.0, 10.0),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(SnakeHead);
+}
+
+fn snake_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut head_positions: Query<&mut Transform, With<SnakeHead>>,
+) {
+    for mut transform in head_positions.iter_mut() {
+        if keyboard_input.pressed(KeyCode::Left) {
+            transform.translation.x -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Right) {
+            transform.translation.x += 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Down) {
+            transform.translation.y -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Up) {
+            transform.translation.y += 2.;
+        }
+    }
+}
