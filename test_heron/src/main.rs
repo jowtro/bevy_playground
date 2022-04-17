30c3a2e781f766b480916837af8e74292ed544d4
Jonnas Costa <jonn567@gmail.com>
2022-04-17 17:57:32 +0100

    add test_heron to the project

---
+++ test_heron/src/main.rs
@@ -0,0 +1,61 @@
+use bevy::prelude::*;
+use heron::prelude::*;
+
+#[bevy_main]
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(PhysicsPlugin::default()) // Add the Heron plugin
+        .add_startup_system(setup_camera)
+        .insert_resource(Gravity::from(Vec3::new(0.0, -300.0, 0.0))) // Define gravity
+        .add_startup_system(spawn_ground)
+        .add_startup_system(spawn)
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+fn spawn_ground(mut commands: Commands) {
+    let size = Vec2::new(200., 30.);
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: Color::BLUE,
+                custom_size: Some(size),
+                ..Default::default()
+            },
+            transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
+            ..Default::default()
+        })
+        .insert(RigidBody::Static)
+        .insert(CollisionShape::Cuboid {
+            // let the size be consistent with our sprite
+            half_extends: size.extend(0.0) / 2.0,
+            border_radius: None,
+        });
+}
+fn spawn(mut commands: Commands) {
+    // the size of our sprite
+    let size = Vec2::new(30.0, 30.0);
+    commands
+        //  here we add a Sprite. We can add any bundle of our choice; the
+        // only required component is a GlobalTransform
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: Color::GREEN,
+                custom_size: Some(size),
+                ..Default::default()
+            },
+            transform: Transform::from_translation(Vec3::new(0.0, 200.0, 0.0)),
+            ..Default::default()
+        })
+        // Make it a physics body, by adding the RigidBody component
+        .insert(RigidBody::Dynamic)
+        // Attach a collision shape
+        .insert(CollisionShape::Cuboid {
+            // let the size be consistent with our sprite
+            half_extends: size.extend(0.0) / 2.0,
+            border_radius: None,
+        });
+}
